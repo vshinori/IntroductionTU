aaeb9d79ffec0c22f6c64d85940d98dc09f5073a
Kévin Roussel <roussel@intechinfo.fr>
2022-11-04 10:54:40 +0100

    TU

---
+++ Assets/Health.cs
@@ -0,0 +1,12 @@
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+
+public struct Health
+{
+    
+
+}
