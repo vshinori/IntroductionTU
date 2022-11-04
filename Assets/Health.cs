41b3d85379ca47a27220804bae9bd824c6701cfc
Kévin Roussel <roussel@intechinfo.fr>
2022-11-04 10:55:32 +0100

    correction

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -7,6 +7,42 @@ using UnityEngine.Events;
 
 public struct Health
 {
-    
+    [SerializeField] int _currentHealth;
+    [SerializeField] int _maxHealth;
+
+    public int CurrentHealth => _currentHealth;
+    public int MaxHealth => _maxHealth;
+
+    public Health(int maxHealth, int currentHealth)
+    {
+        Assert.IsTrue(maxHealth > 0);
+        Assert.IsTrue(currentHealth >= 0);
+        Assert.IsTrue(maxHealth >= currentHealth);
+
+        _currentHealth = currentHealth;
+        _maxHealth = maxHealth;
+    }
+    public Health(int maxHealth) : this(maxHealth, maxHealth)
+    {
+
+    }
+
+    public bool IsDead => _currentHealth == 0;
+
+    public void TakeDamage(int amount)
+    {
+        Assert.IsTrue(amount >= 0);
+        _currentHealth = Mathf.Clamp(_currentHealth - amount, 0, MaxHealth);
+    }
+    public void Heal(int amount)
+    {
+        Assert.IsTrue(amount >= 0);
+        _currentHealth = Mathf.Clamp(_currentHealth + amount, 0, MaxHealth);
+    }
+    public void OneShot()
+    {
+        _currentHealth = 0;
+    }
+
 
 }
--- Assets/MyVector3.cs
+++ Assets/MyVector3.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public struct MyVector3
 {
-    
+    float x, y, z;
+
+    public float X { get => x; set => x = value; }
+    public float Y { get => y; set => y = value; }
+    public float Z { get => z; set => z = value; }
+
+    public float SqrtMagnitude => x*x + y*y + z*z;
+    public float Magnitude => Mathf.Sqrt(SqrtMagnitude);
+
+    public void Normalize()
+    {
+        var m = Magnitude;
+        x /= m;
+        y /= m;
+        z /= m;
+    }
+
+    public static MyVector3 operator+(MyVector3 left, MyVector3 right)
+    {
+        return new MyVector3(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+        //var resultat = Vector2.zero + Vector3.zero;
+    }
+
+    public static MyVector3 operator*(MyVector3 left, float right)
+    {
+        return new MyVector3(left.X * right, left.Y*right, left.Z*right);
+    }
+
+
+    public MyVector3(float X, float Y, float Z)
+    {
+        x = X;
+        y = Y;
+        z = Z;
+    }
 }
