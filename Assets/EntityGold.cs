2369dab87b6b9a6c5b4d29b6d79a58c918bcda04
LeopoldLecl <115979417+LeopoldLecl@users.noreply.github.com>
2023-01-06 17:11:54 +0100

    Ramasser Objet + détruire

--- Assets/EntityGold.cs
+++ Assets/EntityGold.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EntityGold : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,16 @@ public class EntityGold : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+
+            Destroy(gameObject);
+            GoldUI.instance.AddGold(1);
+        }
     }
 }
--- Assets/GoldUI.cs
+++ Assets/GoldUI.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoldUI : MonoBehaviour
 {
+    public int goldCount;
+    public Text goldCountText;
+    public Text goldCountEnd;
+
+    public static GoldUI instance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,12 @@ public class GoldUI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AddGold(int count)
+    {
+        goldCount += count;
+        goldCountText.text = goldCount.ToString();
+        goldCountEnd.text = goldCount.ToString();
     }
 }
